7385a3ddf267655ed697151cfa955d7e77c7e626
HaidarMestrah <10120319@mu.edu.lb>
2023-08-25 06:30:57 +0300

    Server: Modified models

--- online_books_server/models/users.model.js
+++ online_books_server/models/users.model.js
@@ -1,18 +1,47 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  email: {
-    type: String,
-    index: true,
-    lowercase: true,
-    required: true,
-    unique: true,
-    match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}/,
-  },
-  password: { type: String, required: true, minlength: 6 },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
   following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
 });
 
-const model = mongoose.model("User", UserSchema);
-module.exports = model;
+const bookSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  picture: { type: String },
+  review: { type: String },
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  postedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+});
+
+const followingSchema = new mongoose.Schema({
+  follower: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  following: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+const likeSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  book: { type: mongoose.Schema.Types.ObjectId, ref: "Book", required: true },
+});
+
+const Like = mongoose.model("Like", likeSchema);
+const User = mongoose.model("User", userSchema);
+const Book = mongoose.model("Book", bookSchema);
+const Following = mongoose.model("Following", followingSchema);
+
+module.exports = {
+  Like,
+  User,
+  Book,
+  Following,
+};
